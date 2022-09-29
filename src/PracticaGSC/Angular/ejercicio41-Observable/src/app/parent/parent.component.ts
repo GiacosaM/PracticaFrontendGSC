6c729e92cca46979b1a706c04cb13e03a72a33d3
GiacosaM <martingiacosa@gmail.com>
2022-09-29 18:36:50 -0300

    ejercicio41-Observable

---
+++ src/PracticaGSC/Angular/ejercicio41-Observable/src/app/parent/parent.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { promise } from 'protractor';
+import { of, observable } from 'rxjs';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.css']
+})
+export class ParentComponent implements OnInit {
+
+  obs;
+  result;
+  promise;
+
+  constructor() { 
+    this.obs = of(35);
+  }
+
+  ngOnInit() {
+    
+
+    this.promise = this.obs.toPromise();
+
+  }
+
+  listen() {
+    this.promise.then((val) => {
+      this.result = val;
+    })
+  }
+
+}
