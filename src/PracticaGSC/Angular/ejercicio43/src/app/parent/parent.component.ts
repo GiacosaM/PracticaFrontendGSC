f1d3a47a4a21a44ed4393e3d48b00ac92042ed52
GiacosaM <martingiacosa@gmail.com>
2022-09-29 23:54:03 -0300

    Ejercicio43-Tratar con un flujo de datos

---
+++ src/PracticaGSC/Angular/ejercicio43/src/app/parent/parent.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.css']
+})
+export class ParentComponent implements OnInit {
+
+  subject;
+  output = [];
+ 
+  
+  constructor() {
+    this.subject = new Subject();
+   }
+
+  ngOnInit() {
+    this.subject
+    .subscribe(key => {
+      this.output.push(key);
+      this.output =this.output.map(mayusc =>  mayusc.toUpperCase());
+      
+    });
+  }
+
+  keypress(e) {
+    this.subject.next(e.key);
+  }
+  
+}
